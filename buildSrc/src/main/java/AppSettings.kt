08dfac4873fb443c2f30aa49165a211cd7c29e8c
Ray-Yuan Liu <louis383@gmail.com>
2022-01-01 20:09:57 +0800

    Upgrade room to make Apple silicon machine build-able.

--- buildSrc/src/main/java/AppSettings.kt
+++ buildSrc/src/main/java/AppSettings.kt
@@ -3,6 +3,6 @@ object AppSettings {
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 31
     const val AGP_VERSION = "7.0.2"
-    const val KOTLIN_VERSION = "1.5.30"
+    const val KOTLIN_VERSION = "1.5.32"
     const val DESUGAR_LIB_VERSION = "1.1.5"
 }
