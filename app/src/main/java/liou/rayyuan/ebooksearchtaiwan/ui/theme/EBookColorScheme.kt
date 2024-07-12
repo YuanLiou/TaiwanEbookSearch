bbff3caa42818367a3ed0ea576ce5feb15faa9f6
Ray Yuan Liu <louis383@gmail.com>
2024-07-12 20:43:11 +0800

    Introduce Palette and EBookColorScheme

---
+++ app/src/main/java/liou/rayyuan/ebooksearchtaiwan/ui/theme/EBookColorScheme.kt
@@ -0,0 +1,16 @@
+package liou.rayyuan.ebooksearchtaiwan.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+data class EBookColorScheme(
+    val searchBoxColor: Color,
+    val dividerColor: Color,
+    val editTextHintColor: Color,
+    val editTextInputColor: Color,
+    val reorderListBackgroundColor: Color,
+    val backToTopButtonDrawable: Color,
+    val reorderHandlerItemDrawable: Color,
+    val customTabHeaderColor: Color
+)
